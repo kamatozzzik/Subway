3fd8d0947c3ea1910e6e32e2f60043c34c296b32
Vladislav <mycash_crpt@mail.ru>
2019-03-19 12:56:12 +0300

    save result of reading to a variable

--- src/js/startApp.js
+++ src/js/startApp.js
@@ -5,8 +5,13 @@ import { Route } from './Route';
 
 let fileReader = new FileReader();
 let textInput = document.querySelector('#file-input');
+let subwayData = null;
 
 textInput.addEventListener('change', function(e) {
     file = e.target.files[0];
     fileReader.readAsBinaryString(file);
+});
+
+fileReader.addEventListener('loadend', function() {
+	subwayData = this.result;
 });
