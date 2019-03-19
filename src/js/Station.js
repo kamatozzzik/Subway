734a768ea2d4b5407838f4a818d1f2330111017a
Vladislav <mycash_crpt@mail.ru>
2019-03-19 14:28:36 +0300

    add get/set methods

--- src/js/Station.js
+++ src/js/Station.js
@@ -1,6 +1,14 @@
 export class Station {
     constructor(name) {
         this.name = name;
-        this.siblings = [];
+        this._siblings = [];
+    }
+
+    setSibling(sibling) {
+        this._siblings.push(sibling);
+    }
+
+    getSiblings() {
+        return this._siblings;
     }
 }
