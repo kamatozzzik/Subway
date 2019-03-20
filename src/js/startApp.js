320a29ed939b1cef32d7e14398fc3b566031fc3b
Vladislav <mycash_crpt@mail.ru>
2019-03-20 15:19:55 +0300

    add events for creating object struct

--- src/js/startApp.js
+++ src/js/startApp.js
@@ -2,16 +2,39 @@ import { createSubway } from './subway func'
 import { Parser } from './Parser'
 
 let fileReader = new FileReader();
+let parser = new Parser();
 let textInput = document.querySelector('#file-input');
+let currentSubway = null; 
 let subwayData = null;
+const lsKeys = {
+    data: 'data'
+}
 
 textInput.addEventListener('change', function(e) {
-    const file = e.target.files[0];
+    let file = e.target.files[0];
     fileReader.readAsBinaryString(file);
 });
 
 fileReader.addEventListener('loadend', function() {
     subwayData = this.result;
+    subwayData = parser.handleToArray(subwayData);
+    subwayData = parser.handleToSubway(subwayData);
+
+    let storageObj = JSON.stringify(subwayData);
+    localStorage.setItem(lsKeys.data, storageObj);
+
+    currentSubway = createSubway(subwayData);
+
 });
+
+window.addEventListener('load', () => {
+    let lsData = localStorage.getItem(lsKeys.data);
+
+    if (lsData) {
+        subwayData = JSON.parse(lsData);
+	    currentSubway = createSubway(subwayData);
+    }
+});
+
 
 
