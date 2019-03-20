27d0e8c033c8e07073b6e20ef9018e5e2ba90014
Vladislav <mycash_crpt@mail.ru>
2019-03-20 09:14:21 +0300

    fix tabs

--- src/js/SubwayLine.js
+++ src/js/SubwayLine.js
@@ -1,18 +1,17 @@
 class SubwayLine {
-	constructor(name, stations) {
-		this.name = name;
-		this.stations = stations;
-		// this.connections = [];
-	}
+    constructor(name, stations) {
+    this.name = name;
+    this.stations = stations;
+    }
 
-	isCicle() {
-		let last = stations.length - 1;
-		return this.stations[0] === stations[last];
-	}
+    isCicle() {
+        let last = stations.length - 1;
+        return this.stations[0] === stations[last];
+    }
 
-	hasStation(stationName) {
-		return this.stations.some(station => {
-			return stationName === station.name ? true : false;
-		});
-	}
+    hasStation(stationName) {
+        return this.stations.some(station => {
+            return stationName === station.name ? true : false;
+        });
+    }
 }
