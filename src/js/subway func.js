e2a71927cd4dad20bf3b275d82a12095b010e78c
Vladislav <mycash_crpt@mail.ru>
2019-03-20 15:20:13 +0300

    add functions for create objects

---
+++ src/js/subway func.js
@@ -0,0 +1,48 @@
+import { Station } from './Station';
+import { SubwayLine } from './SubwayLine';
+import { Subway } from './Subway';
+
+export function createSubway(data) {
+    let stationNames = data.stationNames.concat(),
+    stationList = [],
+    lines = data.lineNames.concat(),
+    allLines = [];
+
+    for (let i = 0; i < stationNames.length; i++) {
+        for (let j = 0; j < stationNames[i].length; j++) {
+            if (!stationList.includes(stationNames[i][j]))
+                stationList.push(stationNames[i][j]);
+        }
+    }
+
+    stationList = stationList.map(name => {
+        return createStation(name);
+    });
+
+    for (let i = 0; i < lines.length; i++) {
+        let lineStations = [];
+        stationNames[i].forEach(name => {
+            stationList.forEach(station => {
+                if (name === station.name) {
+                    lineStations.push(station);
+                }
+            });
+        });
+
+        allLines[i] = createLine(lines[i], lineStations);
+        lineStations = [];
+    }
+
+    return new Subway(allLines);
+}
+
+function createLine(subwayName, stations) {
+    let currentStations = stations.concat();
+    let line = new SubwayLine(subwayName,currentStations);
+    return line;
+}
+
+function createStation(name) {
+    let station = new Station(name);
+    return station;
+}
